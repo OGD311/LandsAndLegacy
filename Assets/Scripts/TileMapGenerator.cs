7bb3696b734e76d27cb6d024992c6639597a760e
Oliver Goodwin-Day <OliverGD@Olivers-Razer-Blade-15.local>
2023-02-23 16:16:04 +0000

    Fourth commit master branch

--- Assets/Scripts/TileMapGenerator.cs
+++ Assets/Scripts/TileMapGenerator.cs
@@ -10,8 +10,7 @@ public class TileMapGenerator : MonoBehaviour
     public Tile[] bottomTiles;
     public int mapSizeX = 10;
     public int mapSizeY = 10;
-    public int minHeight = 1;
-    public int maxHeight = 5;
+    public float scale = 10f;
 
     private Tilemap tilemap;
 
@@ -23,23 +22,43 @@ public class TileMapGenerator : MonoBehaviour
 
     void GenerateMap()
     {
-        for (int x = 0; x < mapSizeX; x++)
+        int stoneDepth = Random.Range(20, (mapSizeY / 3)+40);
+        for (int x = 0; x < mapSizeX; x++) 
         {
-            int columnHeight = Random.Range(minHeight, maxHeight + 1);
+            int prevStoneDepth = stoneDepth + Random.Range(-4,4);
+            int grassLevel = (int)((mapSizeY-(mapSizeY/3)) + Random.Range(-2,2));
+            int mudDepth = grassLevel - stoneDepth;
+
             for (int y = 0; y < mapSizeY; y++)
             {
                 Tile tile = null;
-                if (y == mapSizeY - 1)
-                {
-                    tile = topTiles[Random.Range(0, topTiles.Length)];
+                if (y < prevStoneDepth)
+                {   
+                    int oreGen = Random.Range(1,100);
+                    if (oreGen < 90){
+                        tile = bottomTiles[0];
+                    }
+                    else if (oreGen >= 90 && oreGen < 94){
+                        tile = bottomTiles[1];
+                    }
+                     //Random.Range(0, bottomTiles.Length)
+                    else if (oreGen >= 94 && oreGen < 97){
+                        tile = bottomTiles[2];
+                    }  
+                    else if (oreGen >= 97 && oreGen < 99){
+                        tile = bottomTiles[3];
+                    }  
+                    else if (oreGen >= 99 && oreGen < 101){
+                        tile = bottomTiles[4];
+                    }  
                 }
-                else if (y == 0)
-                {
-                    tile = bottomTiles[Random.Range(0, bottomTiles.Length)];
-                }
-                else if (y >= mapSizeY - columnHeight)
+                else if (y < grassLevel )
                 {
                     tile = middleTiles[Random.Range(0, middleTiles.Length)];
+                }
+                else if (y == grassLevel)
+                {
+                    tile = topTiles[Random.Range(0, topTiles.Length)];
                 }
                 tilemap.SetTile(new Vector3Int(x, y, 0), tile);
             }
