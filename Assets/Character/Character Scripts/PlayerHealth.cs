3decc843b428ab8c49e8e57c67aba011a5567abf
Oliver GD <114223604+OGD311@users.noreply.github.com>
2024-01-18 16:52:43 +0000

    Update PlayerHealth.cs
    Signed-off-by: Oliver GD <114223604+OGD311@users.noreply.github.com>

--- Assets/Character/Character Scripts/PlayerHealth.cs
+++ Assets/Character/Character Scripts/PlayerHealth.cs
@@ -16,25 +16,18 @@ public class PlayerHealth : MonoBehaviour
         HeartIcon.fillAmount = (health / 100); // Initialise fill (if health taken from save file)
     }
 
-    public void Damage(float Damage){ // Update the health icon
-        //print("Taking "+Damage+" Damage");
-        health = health - Damage;
+    public void UpdateHealth(float amount){ // Update the health icon
+        health = health - amount;
         health = Mathf.Clamp(health,0,100); // Clamp health between 100 (Full health) and 0 (Dead)
         HeartIcon.fillAmount = (health / 100); // Image fill amount is between 1 and 0 
     }
 
-    public void Heal(float HealValue){
-        //print("Healing "+HealValue+" health");
-        health = health + HealValue;
-        health = Mathf.Clamp(health,0,100); // Clamp health between 100 (Full health) and 0 (Dead)
-        HeartIcon.fillAmount = (health / 100); // Image fill amount is between 1 and 0
-    }
 
     void FixedUpdate(){
         if (health <= 0){
             GameSaves.LoadFromSave(PlayerPrefs.GetString("worldName")); //Load the scene
         }
-        Heal(0.01f);// If not dead, then heal a small amount of health
+        UpdateHealth(0.01f);// If not dead, then heal a small amount of health
     }
 
 
